5b136ed5864d423f356bef627a0ef2dabb202ac8
Leyson Castaño <leysofcp@hotmail.com>
2025-03-09 23:43:46 -0500

    se finaliza tema 2

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,7 +5,198 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Tema 2
+      h1 Propulsores hidráulicos
+
+    
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-7
+        p(data-aos="fade-up").mb-4 La calibración de las válvulas de un motor, deberá ser realizada con cierta frecuencia, en periodos que serán dependientes de factores como el desgaste normal de las partes, asentamientos entre válvulas y asientos, envejecimiento del motor, etc.
+
+        .bg-color-3.px-4.py-3.mb-4
+          .row.justify-content-center.align-items-center
+            .col-lg-auto.d-none.d-lg-block
+              figure
+                img(src="@/assets/curso/tema2/icon-1.svg", alt="alt", style="max-width: 190px;")
+
+            .col-lg
+              p(data-aos="fade-up")
+                | Para evitar calibrar las válvulas y, sobre todo, para permitir el funcionamiento mucho más prolongado del motor exento de mantenimientos, se han diseñado algunos sistemas automáticos de regulación, basados todos ellos en sistemas hidráulicos de propulsión.
+
+
+        p(data-aos="fade-up").mb-4 Este sistema de ajuste automático lo conforma el propulsor, el mismo que tiene instalado un pistón dentro de su cuerpo. En la cámara que se forma entre el propulsor y el pistón ingresa la presión de aceite que lubrica a las partes del motor y sirve de ‘colchón’ hidráulico entre ellos; empujando al pistón para que tope con la varilla propulsora, el balancín o directamente sobre la válvula, dependiendo del sistema utilizado.
+
+        figure
+          img(src="@/assets/curso/tema2/1.png", alt="alt")
+
+      .col-lg-5
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 4.
+          span Propulsores hidráulicos
+
+        .bg-color-1.p-4.mb-1
+          figure
+            img(src='@/assets/curso/tema2/f4.png', alt='Imagen que presenta un propulsor hidráulico.', style="max-width: 362px;", data-aos="zoom-in").mx-auto
+
+    .bg-full-width.bg-color-5
+      .p-4
+        .row.justify-content-center.align-items-center
+          .col-lg-auto
+            figure
+              img(src="@/assets/curso/tema2/icon-2.svg", alt="", style="max-width: 90px;", data-aos="zoom-in").mx-auto.mb-4.mb-lg-0
+          .col-lg
+            p(data-aos="fade-up").mb-0.text-white
+              | La calibración de las válvulas de un motor, deberá ser realizada con cierta frecuencia. 
+
+
+    Separador
+
+
+    .titulo-segundo.color-primario
+      h2 2.1 Diseño del propulsor hidráulico
+
+          
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-7
+        p(data-aos="fade-up").mb-4 El cuerpo del propulsor es el encargado de recibir la propulsión del eje de levas. Internamente se ha instalado un pistón que puede deslizarse dentro del cuerpo y que será el encargado de empujar a la varilla propulsora, al balancín o directamente a la válvula.
+
+        .bg-color-8.px-4.py-3.mb-4
+          p(data-aos="fade-up")
+            | Entre el cuerpo y el pistón se ha instalado un muelle, encargado de empujar al pistón para que se mantenga en contacto con la varilla o balancín y permita adicionalmente el ingreso del aceite en la cámara; aceite que viene de los duetos de lubricación del bloque de cilindros o de la culata, lugar en el cual se han instalado los propulsores. Este aceite ingresa por un orificio del cuerpo hasta la cámara del propulsor, a través de una válvula check, la misma que permite ingresar al aceite, pero no lo permite salir.
+
+
+        p(data-aos="fade-up").mb-4 El aceite comprimido transmite de forma amortiguada el empuje de la leva hasta el pistón y este hasta la válvula, por medio de la varilla o balancín, manteniéndose una holgura justa entre elementos de propulsión.
+
+        figure
+          img(src="@/assets/curso/tema2/2.png", alt="alt")
+
+      .col-lg-5
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 5.
+          span Despiece del propulsor tradicional 
+
+        .bg-color-1.p-4.mb-1
+          figure
+            img(src='@/assets/curso/tema2/f5.png', alt='Imagen que presenta el despiece del propulsor tradicional.', style="max-width: 447px;", data-aos="zoom-in").mx-auto
+
+
+    Separador
+
+
+    .titulo-segundo.color-primario
+      h2 2.2 Trabajo del propulsor hidráulico
+
+          
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-7
+        p(data-aos="fade-up").mb-4 El propulsor hidráulico, dependiendo del sistema utilizado en cada motor de impulsión del eje de levas y propulsión que llegue hasta las válvulas, será instalado en el lugar más adecuado.
+
+        .bg-color-7.px-4.py-3.mb-4
+          p(data-aos="fade-up")
+            | En la figura 6, podemos observar un sistema de propulsión de válvulas, en el cual el eje de levas está instalado sobre las válvulas y es una palanca basculante la encargada de empujar a las válvulas. Sobre el extremo en el cual debería estar el apoyo de la palanca, que en los anteriores motores era un apoyo regulable para calibrar las holguras de válvulas, ha sido instalado un propulsor hidráulico, que reemplaza a este apoyo regulable.
+
+
+        figure
+          img(src="@/assets/curso/tema2/3.png", alt="alt")
+
+      .col-lg-5
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 6.
+          span Sistema de propulsión de válvulas 
+
+        .bg-color-1.p-4.mb-1
+          figure
+            img(src='@/assets/curso/tema2/f6.png', alt='Imagen que presenta un sistema de propulsión de válvulas.', style="max-width: 432px;", data-aos="zoom-in").mx-auto
+
+
+    .bg-full-width.bg-color-5.mb-5
+      .p-4
+        .row.justify-content-center.align-items-center
+          .col-lg-auto
+            figure
+              img(src="@/assets/curso/tema2/icon-2.svg", alt="", style="max-width: 90px;", data-aos="zoom-in").mx-auto.mb-4.mb-lg-0
+          .col-lg
+            p(data-aos="fade-up").mb-2.text-white
+              | La presión de aceite que llega hasta los conductos de lubricación se encarga de lubricar al propulsor y de enviar esta presión a su pistón interno para cargarlo, de tal manera que la palanca basculante no tendrá una holgura mayor que la formada por el colchón hidráulico formado.
+            p(data-aos="fade-up").mb-0.text-white
+              | #[strong La presión interna se mantiene, aunque el motor esté apagado gracias al cierre de las válvulas check. ]
+
+
+    .titulo-segundo.color-primario
+      h2 2.3 Propulsor hidráulico dentro de balancín
+
+          
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-7
+        p(data-aos="fade-up").mb-4 Otros diseños de motores modernos utilizan propulsores hidráulicos instalados dentro de uno de los extremos del mismo balancín, es decir, formando prácticamente un solo cuerpo con él.
+
+        .bg-color-8.px-4.py-3.mb-4
+          p(data-aos="fade-up")
+            | La presión hidráulica que se necesita para cargar la cámara interna de cada propulsor se la consigue con la misma presión de aceite que llega hasta el eje de los balancines. Este eje hueco se encarga de repartir la presión del aceite para lubricar los puntos de pivoteo del balancín y por medio de un orifico el aceite ingresa hasta el propulsor. Cuando el propulsor se carga de esta presión, la posible holgura que pudo haber tenido se reduce a una holgura máxima que la mantiene el colchón hidráulico dentro del propulsor.
+
+
+        p(data-aos="fade-up").mb-4 Tales propulsores disponen de una válvula check, la cual permite el ingreso del aceite y no su retorno, por lo mismo, no permitirán la descarga del aceite, manteniéndose el propulsor del balancín cargado permanentemente, inclusive después de que la bomba de aceite del motor ya no provea de presión cuando este se apaga.
+
+
+      .col-lg-5
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 7.
+          span Propulsor hidráulico dentro de balancín
+
+        .bg-color-1.p-4.mb-1
+          figure
+            img(src='@/assets/curso/tema2/f7.png', alt='Imagen que presenta  un propulsor hidráulico instalado dentro de un balancín.', style="max-width: 465px;", data-aos="zoom-in").mx-auto
+
+    
+    .bg-full-width.bg-color-5
+      .p-4
+        .row.justify-content-center.align-items-center
+          .col-lg-auto
+            figure
+              img(src="@/assets/curso/tema2/icon-2.svg", alt="", style="max-width: 90px;", data-aos="zoom-in").mx-auto.mb-4.mb-lg-0
+          .col-lg
+            p(data-aos="fade-up").mb-2.text-white
+              | La presión hidráulica que se necesita para cargar la cámara interna de cada propulsor se la consigue con la misma presión de aceite que llega hasta el eje de los balancines. 
+
+    Separador
+
+    .titulo-segundo.color-primario
+      h2 2.4 Propulsor hidráulico dentro del vaso propulsor
+
+          
+    .row.justify-content-center.align-items-center.mb-4
+      .col-lg-7
+
+
+        .bg-color-6.px-4.py-3.mb-4
+          p(data-aos="fade-up")
+            | El sistema de propulsores que impulsan directamente a la válvula cuando son empujados por la leva también ha sido rediseñado, ya que dentro de este propulsor también se ha instalado un propulsor hidráulico.
+
+
+        p(data-aos="fade-up").mb-4 El propulsor externo recibe de igual manera la presión de aceite de la bomba del motor para lubricarlo, y esta misma presión ingresa por un orificio de su pared lateral. La presión ingresa a su vez hasta el pistón interno del propulsor, cargándolo de presión, obligándolo a apoyarse sobre el vástago de la válvula, manteniendo de igual manera la holgura máxima que permite el colchón hidráulico.
+
+        figure
+          img(src="@/assets/curso/tema2/4.png", alt="alt")
+
+
+      .col-lg-5
+        .titulo-sexto.color-acento-contenido.mb-3
+          h5 Figura 8.
+          span Propulsor hidráulico dentro del vaso propulsor
+
+        .bg-color-1.p-4.py-lg-5.mb-1
+          figure
+            img(src='@/assets/curso/tema2/f8.png', alt='Imagen que presenta  un propulsor hidráulico instalado dentro de un balancín.', style="max-width: 465px;", data-aos="zoom-in").mx-auto
+
+    
+    .bg-full-width.bg-color-5
+      .p-4
+        .row.justify-content-center.align-items-center
+          .col-lg-auto
+            figure
+              img(src="@/assets/curso/tema2/icon-2.svg", alt="", style="max-width: 90px;", data-aos="zoom-in").mx-auto.mb-4.mb-lg-0
+          .col-lg
+            p(data-aos="fade-up").mb-2.text-white
+              | Como la presión mantiene una holgura mínima determinada, el propulsor estará recibiendo el empuje del perfil de la leva y en su extremo bajo impulsará al vástago de la válvula. 
 
 </template>
 
