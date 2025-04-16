3c41253a9ff7289beea4d18b5a69909e1db4de5d
Leyson Castaño <leysofcp@hotmail.com>
2025-04-15 23:47:51 -0500

    se cargan fuentes

--- src/config/global.js
+++ src/config/global.js
@@ -169,7 +169,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/86120369_CF04_DU.pdf',
       },
       {
         icono: 'fas fa-download',
